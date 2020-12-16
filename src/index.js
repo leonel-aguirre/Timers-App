24901f096f293958f1338551b5859701a6b4fb04
NoisyApple <leonel.aguirre77@gmail.com>
2020-12-16 03:47:53 -0600

    Loader added.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
 
-// ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector("#root"));
